d0addca6a7653708feecc7676ee7432861fda733
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-08-13 10:27:25 +0700

    02_02e-Working with events

--- app/component.app.ts
+++ app/component.app.ts
@@ -9,6 +9,20 @@ export class AppComponent {
     name: string;
     artists: Object[];
 
+    onClick(e){
+        this.name  = e.target.innerHTML;
+    }
+
+    addArtist(value){
+        if (value!== '')
+        {
+            this.artists.push({
+                name:value,
+                school : ''
+            });
+        }
+    }
+
     constructor() {
         this.name = 'Boonkuae Boonsutta';
         this.artists = [
